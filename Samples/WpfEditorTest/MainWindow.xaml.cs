c3ff28168a195d640250b71e57e781dd787d34a2
fyodorovvictor <fyodorovvictor@gmail.com>
2018-10-08 16:02:59 +0300

    update

---
+++ Samples/WpfEditorTest/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Fusion.Engine.Common;
+using GISTest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEditorTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var engine = new Game("TestGame");
+
+			engine.GameServer		= new CustomGameServer(engine);
+			engine.GameClient		= new CustomGameClient(engine);
+			engine.GameInterface	= new CustomGameInterface(engine);
+
+			engine.LoadConfiguration("Config.ini");
+
+			engine.RenderSystem.StereoMode = Fusion.Engine.Graphics.StereoMode.WpfEditor;
+			engine.RenderSystem.Width	= 1920;
+			engine.RenderSystem.Height	= 1080;
+			engine.RenderSystem.VSyncInterval = 1;
+
+			//var t = Directory.GetCurrentDirectory();
+			Directory.SetCurrentDirectory(@"..\..\..\..\GISTest\bin\x64\Debug");
+			engine.InitExternal();
+
+			DxElem.Renderer = engine;
+		}
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			DxElem.HandleInput(this);
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			DxElem.Renderer.Invoker.Push("GlobeToSpb");
+		}
+	}
+}
