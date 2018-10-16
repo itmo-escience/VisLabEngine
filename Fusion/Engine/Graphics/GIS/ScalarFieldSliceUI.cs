6ac23a3399a320337da04e97822df164c2fb9a72
Andronnix <a.d.kokorev@yandex.ru>
2018-10-16 17:22:53 +0300

    Slice UI Layers

---
+++ Fusion/Engine/Graphics/GIS/ScalarFieldSliceUI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+using Fusion.Engine.Common;
+using Fusion.Engine.Graphics.GIS.GlobeMath;
+
+namespace Fusion.Engine.Graphics.GIS
+{
+    public class ScalarFieldSliceUI : Gis.GisLayer
+    {       
+        private readonly LinesGisLayer _lines;
+        private readonly TextLabelGisLayer _labels;
+        private readonly List<SliceUI> _slices = new List<SliceUI>();
+
+        public ScalarFieldSliceUI(Game engine, GlobeCamera camera, SpriteFont labelFont, SpriteLayer layer) : base(engine)
+        {
+            _lines = new LinesGisLayer(engine, 2);
+            _labels = new TextLabelGisLayer(engine, labelFont, layer, camera);
+        }
+
+        public SliceUI Add(List<Tuple<DVector3, string>> XAxis, List<Tuple<DVector3, string>> YAxis)
+        {
+            var result = new SliceUI();
+            
+            //_lines.AddLine(XAxis);
+            foreach (var p in XAxis)
+            {
+                var l = _labels.AddLabel(new TextLabelGisLayer.TextLabel(p.Item1, p.Item2, Color.AliceBlue, Color.Black));
+
+                result.Labels.Add(l);
+            }
+
+            foreach (var p in YAxis)
+            {
+                var l = _labels.AddLabel(new TextLabelGisLayer.TextLabel(p.Item1, p.Item2, Color.Green, Color.Black));
+
+                result.Labels.Add(l);
+            }
+
+            _slices.Add(result);
+            return result;
+        }
+
+        public void Remove(SliceUI toBeRemoved)
+        {
+            foreach (var label in toBeRemoved.Labels)
+            {
+                _labels.RemoveLabel(label);
+            }
+
+            _slices.Remove(toBeRemoved);
+        }
+
+        public override void Draw(GameTime gameTime, ConstantBuffer constBuffer)
+        {
+            _lines.Draw(gameTime, constBuffer);
+            _labels.Draw(gameTime, constBuffer);
+
+            base.Draw(gameTime, constBuffer);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _lines.Update(gameTime);
+            _labels.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+    }
+
+    public class SliceUI
+    {
+        public List<TextLabelGisLayer.TextLabel> Labels = new List<TextLabelGisLayer.TextLabel>();
+    }
+}
---
+++ Fusion/Engine/Graphics/GIS/TextLabelGisLayer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+using Fusion.Engine.Common;
+using Fusion.Engine.Graphics.GIS.GlobeMath;
+
+namespace Fusion.Engine.Graphics.GIS
+{
+    public class TextLabelGisLayer : Gis.GisLayer
+    {
+        public class TextLabel
+        {
+            public DVector3 Position;
+            public Color TextColor;
+            public Color BackgroundColor;
+            public string Text;
+            public float Angle;
+            public bool Visible = true;
+
+            public TextLabel(DVector3 position, string text, Color textColor) : this(position, text, textColor, backgroundColor: Color.Zero, angle: 0) { }
+
+            public TextLabel(DVector3 position, string text, Color textColor, Color backgroundColor)
+                : this(position, text, textColor, backgroundColor, angle: 0) { }
+
+            public TextLabel(DVector3 position, string text, Color textColor, Color backgroundColor, float angle)
+            {
+                Position = position;
+                Text = text;
+                TextColor = textColor;
+                BackgroundColor = backgroundColor;
+                Angle = angle;
+            }
+        }
+
+        public double MaxVisibleDistance = 1000;
+
+        private readonly SpriteFont _font;
+        private readonly SpriteLayer _spriteLayer;
+        private readonly GlobeCamera _camera;
+        private readonly List<TextLabel> _labels = new List<TextLabel>();
+
+        public TextLabelGisLayer(Game engine, SpriteFont font, SpriteLayer layer, GlobeCamera camera) : base(engine)
+        {
+            _font = font;
+            _spriteLayer = layer;
+            _camera = camera;
+        }
+
+        public override void Draw(GameTime gameTime, ConstantBuffer constBuffer)
+        {
+            _spriteLayer.Clear();
+
+            var whiteTex = Game.RenderSystem.WhiteTexture;
+            var sortedLabels = _labels
+                .Where(l => l.Visible)
+                .Select(l => new Tuple<TextLabel, double>(l, (l.Position - _camera.FinalCamPosition).Length()))
+                .Where(t => t.Item2 < MaxVisibleDistance)
+                .OrderBy(t => -t.Item2)
+                .Select(t => t.Item1);
+
+            foreach (var label in sortedLabels)
+            {
+                var screenPos = _camera.CartesianToScreen(label.Position);
+                
+                var textRect = _font.MeasureStringF(label.Text);
+
+                _spriteLayer.DrawSprite(whiteTex, 
+                    screenPos.X, screenPos.Y, 
+                    textRect.Width, textRect.Height, 
+                    0, label.BackgroundColor
+                );
+
+                _font.DrawString(
+                    _spriteLayer,
+                    label.Text,
+                    screenPos.X - textRect.Width / 2,
+                    screenPos.Y - textRect.Height / 2,
+                    label.TextColor,
+                    useBaseLine: false
+                );
+            }
+        }
+
+        public TextLabel AddLabel(DVector3 position, string text, Color color)
+        {
+            var label = new TextLabel(position, text, color);
+            _labels.Add(label);
+
+            return label;
+        }
+
+        public TextLabel AddLabel(TextLabel label)
+        {           
+            _labels.Add(label);
+
+            return label;
+        }
+
+        public void RemoveLabel(TextLabel label)
+        {
+            _labels.Remove(label);
+        }
+
+        public void Clear()
+        {
+            _labels.Clear();
+        }
+    }
+}
