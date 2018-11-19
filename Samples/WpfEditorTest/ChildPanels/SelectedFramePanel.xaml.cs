3c38d069525f3ff67c6e8cf812bed5b52bd698b0
fyodorovvictor <fyodorovvictor@gmail.com>
2018-11-19 18:02:37 +0300

    Added visuals to indicate anchors

--- Samples/WpfEditorTest/ChildPanels/SelectedFramePanel.xaml.cs
+++ Samples/WpfEditorTest/ChildPanels/SelectedFramePanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fusion.Engine.Frames;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -60,6 +61,9 @@ namespace WpfEditorTest.ChildPanels
 					{
 						drag.RenderTransform = new TranslateTransform(0, 0);
 					}
+
+					this.UpdateVisualAnchors(this._selectedframe.Anchor);
+
 					_selectedframe.PropertyChanged += ( s, e ) => {
 						if (!this.locked)
 						{
@@ -78,14 +82,27 @@ namespace WpfEditorTest.ChildPanels
 								case "X":
 								case "Y":
 									{
-										var frameDelta = new TranslateTransform(this._selectedframe.X - this._oldX, this._selectedframe.Y - this._oldY);
+										//var frameDelta = new TranslateTransform(this._selectedframe.GlobalRectangle.X - this.RenderTransform.Value.OffsetX, this._selectedframe.GlobalRectangle.Y - this.RenderTransform.Value.OffsetY);
 
-										var group = new TransformGroup();
-										group.Children.Add(this._previousTransform);
-										group.Children.Add(delta);
+										//var group = new TransformGroup();
+										//group.Children.Add(this._previousTransform);
+										//group.Children.Add(delta);
 
-										this.RenderTransform = group;
+										//this.RenderTransform = group;
+										//this._previousTransform = this.RenderTransform;
+										var frameDelta = new TranslateTransform();
+										this.RenderTransform = frameDelta;
+										frameDelta.X = _selectedframe.GlobalRectangle.X;
+										frameDelta.Y = _selectedframe.GlobalRectangle.Y;
 										this._previousTransform = this.RenderTransform;
+
+										_oldX = this.RenderTransform.Value.OffsetX;
+										_oldY = this.RenderTransform.Value.OffsetY;
+										break;
+									}
+								case "Anchor":
+									{
+										this.UpdateVisualAnchors(this._selectedframe.Anchor);
 										break;
 									}
 							} 
@@ -97,6 +114,58 @@ namespace WpfEditorTest.ChildPanels
 			}
 		}
 
+		private void UpdateVisualAnchors( FrameAnchor anchor )
+		{
+			if ((FrameAnchor.Top & anchor) != 0)
+			{
+				this.TopDrag.Background = Brushes.Black;
+				this.TopDrag.BorderBrush = Brushes.White;
+				this.TopAnchorLine.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				this.TopDrag.Background = Brushes.White;
+				this.TopDrag.BorderBrush = Brushes.Black;
+				this.TopAnchorLine.Visibility = Visibility.Collapsed;
+			}
+			if ((FrameAnchor.Bottom & anchor) != 0)
+			{
+				this.BottomDrag.Background = Brushes.Black;
+				this.BottomDrag.BorderBrush = Brushes.White;
+				this.BottomAnchorLine.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				this.BottomDrag.Background = Brushes.White;
+				this.BottomDrag.BorderBrush = Brushes.Black;
+				this.BottomAnchorLine.Visibility = Visibility.Collapsed;
+			}
+			if ((FrameAnchor.Left & anchor) != 0)
+			{
+				this.LeftDrag.Background = Brushes.Black;
+				this.LeftDrag.BorderBrush = Brushes.White;
+				this.LeftAnchorLine.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				this.LeftDrag.Background = Brushes.White;
+				this.LeftDrag.BorderBrush = Brushes.Black;
+				this.LeftAnchorLine.Visibility = Visibility.Collapsed;
+			}
+			if ((FrameAnchor.Right & anchor) != 0)
+			{
+				this.RightDrag.Background = Brushes.Black;
+				this.RightDrag.BorderBrush = Brushes.White;
+				this.RightAnchorLine.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				this.RightDrag.Background = Brushes.White;
+				this.RightDrag.BorderBrush = Brushes.Black;
+				this.RightAnchorLine.Visibility = Visibility.Collapsed;
+			}
+		}
+
 		public Point _previousMouseLocation { get; set; }
 		public Transform _previousDragTransform { get; set; }
 		public Transform _previousTransform { get; set; }
@@ -105,17 +174,28 @@ namespace WpfEditorTest.ChildPanels
 		public bool _dragMousePressed { get; set; }
 		public Window _window { get; set; }
 		public List<Border> drags { get; private set; }
+		public List<Border> visualAnchors { get; private set; }
 
 		public double WidthBuffer {
 			get => _widthBuffer;
 			set { _widthBuffer = value;
-				this.Width = Math.Max(0, _widthBuffer);
+				this.Width = Math.Max(0, _widthBuffer); //Math.Abs(_widthBuffer); 
+
+				//var group = new TransformGroup();
+				//var scale = new ScaleTransform();
+				//scale.ScaleX = Math.Sign(_widthBuffer) * 1;
+				//scale.ScaleY = 1;
+				//group.Children.Add(scale);
+				//group.Children.Add(this.RenderTransform);
+				//this.RenderTransform = group;
 			}
 		}
 		public double HeightBuffer {
 			get => _heightBuffer;
 			set { _heightBuffer = value;
 				this.Height = Math.Max(0, _heightBuffer);
+
+				//((ScaleTransform)(this.RenderTransform)).ScaleY = Math.Sign(_heightBuffer) * 1;
 			}
 		}
 
@@ -136,15 +216,25 @@ namespace WpfEditorTest.ChildPanels
 			}
 			);
 
+			this.visualAnchors = new List<Border>(new Border[]
+			{
+				TopAnchorLine,
+				LeftAnchorLine, RightAnchorLine,
+				BottomAnchorLine,
+			}
+			);
+
 			this.SizeChanged += ( s, e ) =>
 			{
-				this.locked = true;
+
 				if (this._selectedframe != null)
 				{
+					this.locked = true;
 					this._selectedframe.Width = (int)(this.Width + Math.Min(0, WidthBuffer));
-					this._selectedframe.Height = (int)(this.Height + Math.Min(0, HeightBuffer)); 
+					this._selectedframe.Height = (int)(this.Height + Math.Min(0, HeightBuffer));
+					this.locked = false;
 				}
-				this.locked = false;
+
 			};
 			this.LayoutUpdated += ( s, e ) =>
 			{
@@ -163,6 +253,7 @@ namespace WpfEditorTest.ChildPanels
 					//this._selectedframe.Height = (int)(this.Height + Math.Min(0, HeightBuffer));
 					this.locked = false;
 				}
+				this.UpdateAnchorLines();
 			};
 
 			_oldX = this.RenderTransform.Value.OffsetX;
@@ -196,6 +287,38 @@ namespace WpfEditorTest.ChildPanels
 			//			}
 			//	}
 			//};
+		}
+
+		private void UpdateAnchorLines()
+		{
+			if (this._selectedframe != null)
+			{
+				double measure;
+				if (this.TopAnchorLine.Visibility == Visibility.Visible)
+				{
+					measure = this._selectedframe.Y;
+					this.TopAnchorLine.Height = Math.Abs(measure);
+					this.TopAnchorLine.Margin = new Thickness(0, -this.TopAnchorLine.Height * Math.Sign(measure), 0, 9);
+				}
+				if (this.BottomAnchorLine.Visibility == Visibility.Visible)
+				{
+					measure = this._selectedframe.Parent.Height - (this._selectedframe.Y + this._selectedframe.Height);
+					this.BottomAnchorLine.Height = Math.Abs(measure);
+					this.BottomAnchorLine.Margin = new Thickness(0, 9, 0, -this.BottomAnchorLine.Height * Math.Sign(measure));
+				}
+				if (this.LeftAnchorLine.Visibility == Visibility.Visible)
+				{
+					measure = this._selectedframe.X;
+					this.LeftAnchorLine.Width = Math.Abs(measure);
+					this.LeftAnchorLine.Margin = new Thickness(-this.LeftAnchorLine.Width * Math.Sign(measure), 0, 9, 0);
+				}
+				if (this.RightAnchorLine.Visibility == Visibility.Visible)
+				{
+					measure = this._selectedframe.Parent.Width - (this._selectedframe.X + this._selectedframe.Width);
+					this.RightAnchorLine.Width = Math.Abs(measure);
+					this.RightAnchorLine.Margin = new Thickness(9, 0, -this.RightAnchorLine.Width * Math.Sign(measure), 0);
+				} 
+			}
 		}
 
 		private bool PositionChanged()
