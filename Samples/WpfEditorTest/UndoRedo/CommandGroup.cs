d68bb7785f87b38807e28ae480eefefc7ed36d41
fyodorovvictor <fyodorovvictor@gmail.com>
2018-12-05 10:29:47 +0300

    update

---
+++ Samples/WpfEditorTest/UndoRedo/CommandGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEditorTest.UndoRedo
+{
+	class CommandGroup : IEditorCommand
+	{
+		private readonly IEditorCommand[] nestedCommands;
+
+		public CommandGroup(params IEditorCommand[] commands)
+		{
+			nestedCommands = commands;
+		}
+
+		public void Do()
+		{
+			foreach (var command in nestedCommands)
+			{
+				command.Do();
+			}
+		}
+
+		public void Undo()
+		{
+			foreach (var command in nestedCommands.Reverse())
+			{
+				command.Undo();
+			}
+		}
+	}
+}
---
+++ Samples/WpfEditorTest/UndoRedo/CommandManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace WpfEditorTest.UndoRedo
+{
+	public class CommandManager
+	{
+		public static CommandManager Instance { get; } = new CommandManager();
+
+		private CommandManager()
+		{
+
+		}
+
+		private Stack<IEditorCommand> _doCommands = new Stack<IEditorCommand>();
+		private Stack<IEditorCommand> _undoCommands = new Stack<IEditorCommand>();
+
+		public bool TryUndoCommand()
+		{
+			if (_undoCommands.Count==0)
+				return false;
+
+			var movedCommand = _undoCommands.Pop();
+			movedCommand.Undo();
+			_doCommands.Push(movedCommand);
+			return true;
+		}
+
+		public bool TryRedoCommand()
+		{
+			if (_doCommands.Count == 0)
+				return false;
+
+			var movedCommand = _doCommands.Pop();
+			movedCommand.Do();
+			_undoCommands.Push(movedCommand);
+			return true;
+		}
+
+		public void Execute(IEditorCommand command)
+		{
+			command.Do();
+			_undoCommands.Push(command);
+			_doCommands.Clear();
+		}
+
+		public void Reset()
+		{
+			_undoCommands.Clear();
+			_doCommands.Clear();
+		}
+	}
+}
