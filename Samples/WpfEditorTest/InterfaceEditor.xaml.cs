24d846a6f97a18693638ba84b1399c865404027e
fyodorovvictor <fyodorovvictor@gmail.com>
2018-12-18 12:18:45 +0300

    Minor fixes

--- Samples/WpfEditorTest/InterfaceEditor.xaml.cs
+++ Samples/WpfEditorTest/InterfaceEditor.xaml.cs
@@ -187,11 +187,11 @@ namespace WpfEditorTest
 			if (SelectionLayer.frameSeletcionPanelList.Count >= 0)
 			{
 				List<IEditorCommand> commands = new List<IEditorCommand>();
+				commands.Add(new SelectFrameCommand(new List<Frame> { }));
 				foreach (var frame in SelectionLayer.frameSeletcionPanelList.Keys)
 				{
 					commands.Add(new FrameParentChangeCommand(frame, null));
 				}
-				commands.Add(new SelectFrameCommand(new List<Frame> { }));
 				var command = new CommandGroup(commands.ToArray());
 				CommandManager.Instance.Execute(command);
 			}
@@ -404,11 +404,14 @@ namespace WpfEditorTest
 
 		private void ExecutedPasteFrameCmdCommand( object sender, ExecutedRoutedEventArgs e )
 		{
-			List<IEditorCommand> commands = new List<IEditorCommand>();
-			this.AddframeToScene(Fusion.Core.Utils.FrameSerializer.ReadFromString(xmlFrameBuffer),
-				System.Windows.Input.Mouse.GetPosition(this), commands);
-			var command = new CommandGroup(commands.ToArray());
-			CommandManager.Instance.Execute(command);
+			if (!string.IsNullOrEmpty(xmlFrameBuffer))
+			{
+				List<IEditorCommand> commands = new List<IEditorCommand>();
+				this.AddframeToScene(Fusion.Core.Utils.FrameSerializer.ReadFromString(xmlFrameBuffer),
+					System.Windows.Input.Mouse.GetPosition(this), commands);
+				var command = new CommandGroup(commands.ToArray());
+				CommandManager.Instance.Execute(command); 
+			}
 		}
 
 		private void AlwaysCanExecute( object sender, CanExecuteRoutedEventArgs e )
