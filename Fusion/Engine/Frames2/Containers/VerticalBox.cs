ef200a406d2344398b0c92e0704b8f5c238a179a
Vladislav Leonov <vlad_leonov@bk.ru>
2019-01-25 12:05:07 +0300

    Adding VerticalBox layout logic.

--- Fusion/Engine/Frames2/Containers/VerticalBox.cs
+++ Fusion/Engine/Frames2/Containers/VerticalBox.cs
@@ -6,7 +6,13 @@ namespace Fusion.Engine.Frames2.Containers
     {
         public override void Update(GameTime gameTime)
         {
-
+            float bottomBorder = this.Y;
+            foreach (var child in Children)
+            {
+                child.X += this.X - child.BoundingBox.X;
+                child.Y += bottomBorder - child.BoundingBox.Y;
+                bottomBorder += child.BoundingBox.Height;
+            }
         }
 
         public VerticalBox(float x, float y, float width, float height) : base(x, y, width, height)
