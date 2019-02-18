8c58cd6a0b2f155b098f8a9ad0df4b0ad9c29b78
Vladislav Leonov <vlad_leonov@bk.ru>
2019-02-18 19:03:54 +0300

    Adding DebugDraw to FreePlacement.

--- Fusion/Engine/Frames2/Containers/FreePlacement.cs
+++ Fusion/Engine/Frames2/Containers/FreePlacement.cs
@@ -11,12 +11,23 @@ namespace Fusion.Engine.Frames2.Containers
 
         }
 
-        public FreePlacement() : base() { }
+        public FreePlacement() : base() {
+            debugBrush = new SolidBrushD2D(new Color4(0, 1, 1, 1));
+            debugTextFormat = new TextFormatD2D("Consolas", 14);
+        }
 
         public FreePlacement(float x, float y, float width, float height, bool needClipping = false) : base(x, y, width, height, needClipping)
         {
             debugBrush = new SolidBrushD2D(new Color4(0, 1, 1, 1));
             debugTextFormat = new TextFormatD2D("Consolas", 14);
+        }
+
+        public override void DebugDraw(SpriteLayerD2D layer)
+        {
+            base.DebugDraw(layer);
+            layer.Draw(new TransformCommand(GlobalTransform));
+
+            layer.Draw(new Rect(0, 0, Width, Height, debugBrush));
         }
     }
 }
